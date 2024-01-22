c778ee706a59856460ca3ab1d60d8fb1daada2da
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-01-22 11:34:31 +0200

    Changed db name and user login

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -24,8 +24,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
     app.ApplyMigrations();
 }
