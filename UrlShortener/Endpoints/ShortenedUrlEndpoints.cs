32d2ad962aed338922bfee75df3c130af411f167
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-01-28 16:21:38 +0200

    Move endpoints to separate class
    Add ShortenedUrlEndpoints static class with MapShortenedUrlRoutes method to map routes

---
+++ UrlShortener/Endpoints/ShortenedUrlEndpoints.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Entities;
+using UrlShortener.Models;
+using UrlShortener.Repositories.Interfaces;
+using UrlShortener.Services;
+using UrlShortener.Services.Static;
+
+namespace UrlShortener.Endpoints
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ShortenedUrlEndpoints
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        public static void MapShortenedUrlRoutes(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("api/shorten", ShortenUrl);
+            app.MapGet("api/{code}", GetUrlByCode);
+        }
+
+        [HttpPost]
+        private static async Task<IResult> ShortenUrl(
+            ShortenUrlRequest request,
+            UrlShorteningService urlShorteningService,
+            IUnitOfWork unitOfWork,
+            HttpContext httpContext)
+        {
+            if (false == UlrValidationService.IsUrlValid(request.Url, UriKind.Absolute))
+            {
+                return Results.BadRequest("The specified URL is invalid: " + request.Url);
+            }
+
+            var code = await urlShorteningService.GenerateUniqueCode();
+
+            var shortenedUrl = new ShortenedUrl(
+                Guid.NewGuid(),
+                request.Url,
+                $"{httpContext.Request.Scheme}://{httpContext.Request.Host}/api/{code}",
+                code,
+                DateTime.UtcNow
+            );
+
+            unitOfWork.ShortenedUrlRepository.Insert(shortenedUrl);
+
+            await unitOfWork.ShortenedUrlRepository.CommitAsync();
+
+            return Results.Ok(shortenedUrl.ShortUrl);
+        }
+        
+        [HttpGet]
+        private static async Task<IResult> GetUrlByCode(
+            string code,
+            IUnitOfWork unitOfWork,
+            HttpResponse response)
+        {
+            if (false == ShortenedUrlCodeValidatorService.IsCodeValid(code))
+            {
+                return Results.BadRequest(Constants.Constants.InvalidCodeBadRequestMessage);
+            }
+
+            var shortenedUrl = await unitOfWork.ShortenedUrlRepository.GetByCodeAsync(code);
+
+            return shortenedUrl is null ? Results.NotFound() : Results.Redirect(shortenedUrl.LongUrl);
+        }
+    }
+}
--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,22 +1,14 @@
-using Azure;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.Metadata;
 using UrlShortener;
-using UrlShortener.Constants;
-using UrlShortener.Entities;
+using UrlShortener.Endpoints;
 using UrlShortener.Extensions;
-using UrlShortener.Models;
-using UrlShortener.Repositories.Implementations;
-using UrlShortener.Repositories.Interfaces;
-using UrlShortener.Services;
-using UrlShortener.Services.Static;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(o =>
@@ -37,49 +29,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.ApplyMigrations();
 
-app.MapPost("api/shorten", async (
-    ShortenUrlRequest request,
-    UrlShorteningService urlShorteningService,
-    IUnitOfWork unitOfWork,
-    HttpContext httpContext) =>
-{
-    if (false == UlrValidationService.IsUrlValid(request.Url, UriKind.Absolute))
-    {
-        return Results.BadRequest("The specified URL is invalid: " + request.Url);
-    }
-
-    var code = await urlShorteningService.GenerateUniqueCode();
-
-    var shortenedUrl = new ShortenedUrl(
-        Guid.NewGuid(),
-        request.Url,
-        $"{httpContext.Request.Scheme}://{httpContext.Request.Host}/api/{code}",
-        code,
-        DateTime.UtcNow
-        );
-
-    unitOfWork.ShortenedUrlRepository.Insert(shortenedUrl);
-
-    await unitOfWork.ShortenedUrlRepository.CommitAsync();
-
-    return Results.Ok(shortenedUrl.ShortUrl);
-});
-
-app.MapGet("api/{code}", async (string code, IUnitOfWork unitOfWork, HttpResponse response) =>
-{
-    if(false == ShortenedUrlCodeValidatorService.IsCodeValid(code)) 
-    {
-        return Results.BadRequest(Constants.InvalidCodeBadRequestMessage);
-    }
-
-    var shortenedUrl = await unitOfWork.ShortenedUrlRepository.GetByCodeAsync(code);
-
-    return shortenedUrl is null ? Results.NotFound() : Results.Redirect(shortenedUrl.LongUrl);
-});
+app.MapShortenedUrlRoutes();
 
 app.UseHttpsRedirection();
 app.UseCors();
